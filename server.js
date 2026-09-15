c654e9ecf6243caf87f23dc5360b549c7aefc058
Full_stackAPP <sabinmahata550@gmail.com>
2026-09-15 14:46:00 +0545

    change mongourl

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 import app from "./src/app.js";
 import config from "./src/config/config.js";
+import connectDB from "./src/config/db.js";
 
-app.listen(config.port, () => {
-    console.log(`Server running on port ${config.port}`);
-});
+const startServer = async () => {
+    await connectDB();
+
+    app.listen(config.port, () => {
+        console.log(`Server running on port ${config.port}`);
+    });
+};
+
+startServer();
--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 import express from "express";
-import connectDB from "./config/db.js";
-import userRouter from "./routes/userRoute.js";
-import reviewRoutes from "./routes/reviewRoute.js"
-import movieRoutes from "./routes/moviRoute.js"
-import watchRoute from "./routes/watchRoute.js"
-import dotenv from "dotenv"
-dotenv.config();
-const app = express();
-connectDB();
-app.use(express.json());
-app.use("/api/users",userRouter)
 
+import userRouter from "./routes/userRoute.js";
+import reviewRoutes from "./routes/reviewRoute.js";
+import movieRoutes from "./routes/moviRoute.js";
+import watchRoute from "./routes/watchRoute.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/users", userRouter);
 app.use("/api/movies", movieRoutes);
 app.use("/api/reviews", reviewRoutes);
 app.use("/api/watchlist", watchRoute);
+
 export default app;
